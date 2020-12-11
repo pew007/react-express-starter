3c9d2eb80f08c80008fb5567f28f21c92a51116d
Pengyu Wang <bird0407@gmail.com>
2020-12-10 22:12:39 -0800

    configure webpack css loaders

--- webpack/client.config.js
+++ webpack/client.config.js
@@ -2,21 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const appRootPath = path.resolve(__dirname, '..');
+const assetsPath = path.resolve(__dirname, '..', 'public');
+const clientPath = path.resolve(__dirname, '..', 'client');
 
 module.exports = {
     entry: appRootPath + '/client/index.js',
     output: {
-        path: path.join(appRootPath, 'dist'),
-        filename: "bundle.js"
+        path: assetsPath,
+        filename: "bundle.js",
+        publicPath: "/public/",
+        crossOriginLoading: "anonymous",
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
+                include: clientPath,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loader: "file-loader",
             }
         ]
     },
